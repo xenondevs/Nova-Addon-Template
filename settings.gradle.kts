c4cc94fbcd2d1e7a09b279507cade3281e9f07de
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-05-22 17:42:20 +0200

    Bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "example" // TODO: Change this to your addon id
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            version("nova", "0.13-SNAPSHOT")
+            version("nova", "0.13")
             version("spigot", "1.19.4-R0.1-SNAPSHOT")
             
             library("nova", "xyz.xenondevs.nova", "nova").versionRef("nova")
