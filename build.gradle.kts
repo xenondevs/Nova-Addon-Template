5e210aecd500a7083ca19232d5bfe98a3455b8ae
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-08-12 13:48:30 +0200

    Update to Nova 0.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
-import org.gradle.configurationcache.extensions.capitalized
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.example" // TODO: Change this to your group
 version = "1.0-SNAPSHOT" // TODO: Change this to your addon version
-
-val mojangMapped = project.hasProperty("mojang-mapped")
 
 plugins {
     alias(libs.plugins.kotlin)
@@ -24,33 +22,32 @@ dependencies {
 }
 
 addon {
-    id.set(project.name)
-    name.set(project.name.capitalized())
-    version.set(project.version.toString())
-    novaVersion.set(libs.versions.nova)
-    main.set("com.example.ExampleAddon") // TODO: Change this to your main class
-    authors.add("ExampleAuthor") // TODO: Set your list of authors
+    id = project.name
+    name = project.name.replaceFirstChar(Char::uppercase)
+    version = project.version.toString()
+    novaVersion = libs.versions.nova
+    main = "com.example.ExampleAddon" // TODO: Change this to your main class
+    authors = listOf("ExampleAuthor") // TODO: Set your list of authors
 }
 
 tasks {
     register<Copy>("addonJar") {
         group = "build"
-        if (mojangMapped) {
-            dependsOn("jar")
-            from(File(buildDir, "libs/${project.name}-${project.version}-dev.jar"))
-        } else {
-            dependsOn("reobfJar")
-            from(File(buildDir, "libs/${project.name}-${project.version}.jar"))
-        }
-        
-        from(File(project.buildDir, "libs/${project.name}-${project.version}.jar"))
-        into((project.findProperty("outDir") as? String)?.let(::File) ?: project.buildDir)
+        dependsOn("jar")
+        from(File(project.layout.buildDirectory.get().asFile, "libs/${project.name}-${project.version}.jar"))
+        into((project.findProperty("outDir") as? String)?.let(::File) ?: project.layout.buildDirectory.get().asFile)
         rename { "${addonMetadata.get().addonName.get()}-${project.version}.jar" }
     }
     
     withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "17"
+        compilerOptions { 
+            jvmTarget = JvmTarget.JVM_21
         }
+    }
+}
+
+afterEvaluate {
+    tasks.getByName<Jar>("jar") {
+        archiveClassifier = ""
     }
 }
