0a751a93d33100b9453bb6f5df2006fe00a93768
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-12-08 21:15:08 +0100

    Bump Nova version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("xyz.xenondevs.nova:catalog:0.18-alpha.1") // TODO: change this when updating to a newer Nova version
+            from("xyz.xenondevs.nova:catalog:0.18-alpha.2") // TODO: change this when updating to a newer Nova version
         }
     }
 }
